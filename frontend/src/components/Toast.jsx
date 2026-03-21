2b7e26e93a7ff065592178996590daba720312f6
Ethan Nguyen <ethannguyen@vt.edu>
2026-03-21 16:13:08 -0400

    created Toast component

---
+++ frontend/src/components/Toast.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useApp } from '../context/AppContext';
+import './Toast.css';
+
+export default function Toast() {
+  const { toast } = useApp();
+
+  if (!toast) return null;
+
+  return (
+    <div className={`toast toast--${toast.type}`} key={toast.id}>
+      {toast.message}
+    </div>
+  );
+}
